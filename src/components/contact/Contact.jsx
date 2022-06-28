77a14785079672559f8c68ca8a90fb9cfbd45fe3
armstmol01 <armstmol01@gmail.com>
2022-06-28 11:24:30 -0700

    finished

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,25 @@ import './Contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {TbBrandMessenger} from 'react-icons/tb'
 import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_a4c1ahh', 'template_9ai70jw', form.current, 'PCGoNLqyNBc20wHVA')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -32,7 +49,7 @@ const Contact = () => {
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required />
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Footer.css'
+import {FaFacebookSquare, FaTwitter} from 'react-icons/fa'
+import {GrInstagram} from 'react-icons/gr'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Haija Bintu</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://facebook.com'><FaFacebookSquare /></a>
+        <a href='https://instagram.com'><GrInstagram /></a>
+        <a href='https://twitter.com'><FaTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Molly Armstrong. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
